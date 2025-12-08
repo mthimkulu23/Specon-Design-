565125816efd970d03a9783131ec4cbaea9a4973
mthimkulu23 <thabang23mthimkulu@gmail.com>
2025-12-08 15:29:38 +0200

    Fix Flask 404: move static files to root and update template paths

---
+++ MVC/Controllers/main_controller.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+
+main_blueprint = Blueprint('main', __name__)
+
+@main_blueprint.route('/')
+def home():
+    return render_template('Frontend/landing.html')
+
